df5331ea5446152aa29c819838cd0a46c546ce4c
LandChit <78423278+LandChit@users.noreply.github.com>
2022-10-27 19:16:05 +0800

    update

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ def index():
     return render_template('index.html')
 
 #can double as a creation page
-@app.route('/modification')
-def create():
-    return 'modification page'
+@app.route('/start')
+def start():
+    return render_template('start.html')
 
-@app.route('/review')
-def review():
-    return 'reviewing page'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5500, debug=True)
